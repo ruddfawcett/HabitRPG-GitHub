a5dbb87d619287279b1b8435ea2590a3e06a0931
Rudd Fawcett <rudd.fawcett@gmail.com>
2013-03-14 22:53:21 -0300

    Initial commit

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,8 +1,8 @@
 <?php
-	$hostname = "HabitRPGGitHub.db.10270192.hostedresource.com";
-	$username = "HabitRPGGitHub";
-	$password = "HabitRPG1!";
-	$dbname = "HabitRPGGitHub";
+	$hostname = "";
+	$username = "";
+	$password = "";
+	$dbname = "";
 
 	$db= new PDO("mysql:host=".$hostname.";dbname=".$dbname.";charset=utf8", $username, $password);
 ?>
