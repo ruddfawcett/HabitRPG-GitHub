3bf0e2585b48cf49c42926cd8ebf3bb53bc9b6b8
Bradley Wogsland <bradley@wogsland.org>
2014-08-14 18:22:15 -0500

    no one wants to fork an empty repo...

--- changelog.php
+++ changelog.php
@@ -41,8 +41,8 @@
 
       <div class="footer">
         <p>&copy; Rudd Fawcett 2013.  Design directly taken from <a href='http://twitter.github.com/bootstrap/examples/justified-nav.html#'>Bootstrap</a>.</p>
-        <span class='pull-right' style='margin-top: -30px;'><iframe src="http://ghbtns.com/github-btn.html?user=ruddfawcett&repo=HabitRPG-GitHub&type=watch&count=true"
-  allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe><iframe src="http://ghbtns.com/github-btn.html?user=ruddfawcett&repo=HabitRPG-GitHub&type=fork&count=true"
+        <span class='pull-right' style='margin-top: -30px;'><iframe src="http://ghbtns.com/github-btn.html?user=HabitRPG&repo=HabitRPG-GitHub&type=watch&count=true"
+  allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe><iframe src="http://ghbtns.com/github-btn.html?user=HabitRPG&repo=HabitRPG-GitHub&type=fork&count=true"
   allowtransparency="true" frameborder="0" scrolling="0" width="95" height="20"></iframe></span>
       </div>
 
