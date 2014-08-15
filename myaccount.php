4ab5f9551be0fc269975b905668b9fc754c5c0f4
Bradley Wogsland <bradley@wogsland.org>
2014-08-14 19:14:54 -0500

    added header

--- myaccount.php
+++ myaccount.php
@@ -1,17 +1,5 @@
-<?php require('scripts/session.php'); ?>
+<?php require('includes/header.php'); ?>
 <?php require('scripts/connect.php'); ?>
-<!DOCTYPE html>
-<html lang='en'>
-	<head>
-		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
-		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
-		<link href='styles/main.css' rel='stylesheet' type='text/css'>
-		<script src='http://code.jquery.com/jquery-1.9.1.min.js'></script>
-		<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-		<script src='scripts/js/submission.js'></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-		<title>HabitRPG-GitHub</title>
-	<head>
 	<body>
     <div class="container">
 
