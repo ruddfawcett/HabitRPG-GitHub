82872447975685ffa18fe8cc76aafcd251f0ead1
Bradley Wogsland <bradley@wogsland.org>
2014-08-14 19:18:28 -0500

    added footer

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-<?php require('includes/header.php'); ?>
+<?php require('scripts/header.php'); ?>
 	<body>
     <div class="container">
 
@@ -35,15 +35,4 @@
     			</ul>
  -->
 
-      <hr>
-
-      <div class="footer">
-        <p>&copy; Rudd Fawcett 2013.  Design directly taken from <a href='http://twitter.github.com/bootstrap/examples/justified-nav.html#'>Bootstrap</a>.</p>
-        <span class='pull-right' style='margin-top: -30px;'><iframe src="http://ghbtns.com/github-btn.html?user=HabitRPG&repo=HabitRPG-GitHub&type=watch&count=true"
-  allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe><iframe src="http://ghbtns.com/github-btn.html?user=HabitRPG&repo=HabitRPG-GitHub&type=fork&count=true"
-  allowtransparency="true" frameborder="0" scrolling="0" width="95" height="20"></iframe></span>
-      </div>
-
-    </div>
-	</body>
-</html>
+<?php require('scripts/footer.php'); ?>
